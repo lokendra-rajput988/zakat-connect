35c96ae6d0ff387df0bcd0d90b895fbc82fb3138
lokendra-rajput988 <m.lokendrarajput@gmail.com>
2025-07-17 12:02:17 +0530

    created PersonDetailController

---
+++ src/main/java/com/mindprove/zakat/controllers/PersonDetailController.java
@@ -0,0 +1,10 @@
+package com.mindprove.zakat.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zakat-connect/api/person-detail/")
+public class PersonDetailController {
+
+}
