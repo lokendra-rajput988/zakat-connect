2abf77539421f5426a3362fe03331fc2ee0c5641
lokendra-rajput988 <m.lokendrarajput@gmail.com>
2025-07-17 11:48:33 +0530

    created PersonDetail

---
+++ src/main/java/com/mindprove/zakat/entities/PersonDetail.java
@@ -0,0 +1,20 @@
+package com.mindprove.zakat.entities;
+
+import java.util.Date;
+
+public class PersonDetail {
+
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String mobile;
+	private String confirmPassword;
+	private Integer age;
+	private String gender;
+	private String description;
+	private boolean isActive;
+	private Date createdAt;
+	private Date updatedAt;
+}
