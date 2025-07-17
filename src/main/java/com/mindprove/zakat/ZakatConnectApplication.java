3e20ab97463d356aca982440932268abffcafca9
lokendra-rajput988 <m.lokendrarajput@gmail.com>
2025-07-17 11:37:45 +0530

    my fisrt commit

---
+++ src/main/java/com/mindprove/zakat/ZakatConnectApplication.java
@@ -0,0 +1,13 @@
+package com.mindprove.zakat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZakatConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZakatConnectApplication.class, args);
+	}
+
+}
