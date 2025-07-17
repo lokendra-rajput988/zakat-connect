2c0044edd8cb12d848e7c3c8af20bfaa4356389e
lokendra-rajput988 <m.lokendrarajput@gmail.com>
2025-07-17 11:58:17 +0530

    created PersonDetailDto

---
+++ src/main/java/com/mindprove/zakat/dtos/PersonDetailDto.java
@@ -0,0 +1,23 @@
+package com.mindprove.zakat.dtos;
+
+import java.util.Date;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class PersonDetailDto {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String confirmPassword;
+	private String mobile;
+	private Integer age;
+	private String gender;
+	private String description;
+	private boolean isActive;
+	private Date createdAt;
+	private Date updatedAt;
+}
